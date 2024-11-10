68e52ec4d9ae596bdfd67d0942e20bf4b40dc384
aryan1283 <156881580+aryan1283@users.noreply.github.com>
2024-11-10 16:23:37 +0530

    Add files via upload

---
+++ mamaearth/Mamaearth-Clone/Home+All_Products/home.js
@@ -0,0 +1,261 @@
+var bestSeller = [
+    {
+        img:"https://images.mamaearth.in/catalog/product/u/b/ubtan-hair-removal-kit.jpg?format=auto&width=400&height=400",
+        rate:"Rs.",
+    price:449,
+    title:"Ubtan Nourishing Hair Removal Kit & Growth Reduction - 100g",
+    subtitle:"Removes Hair in 8 Minutes | Delays Hair Growth",
+    img2:"",
+    rating:"5 stars |", 
+    img_url:"",
+    review:"40 Reviews",
+    },
+    {
+        img:"https://images.mamaearth.in/catalog/product/r/o/rose-body-lotion_1.jpg?format=auto&width=400&height=400",
+        rate:"Rs.",
+    price:399,
+    title:"Rose Body Lotion with Rose Water and Milk For Deep Hydration - 400ml",
+    subtitle:"Intense Hydration | Non-Greasy | Softens Skin",
+    img2:"",
+    rating:"3.5 stars |",
+    img_url:"",
+    review:"2 Reviews",
+    },
+    {
+        img:"https://images.mamaearth.in/catalog/product/1/-/1-wid-ingredient.jpg?format=auto&width=400&height=400",
+        rate:"Rs.",
+    price:399,
+    title:"Vitamin C Body Lotion with Vitamin C and Honey for Radiant Skin - 400 ml",
+    subtitle:"Deep Hydration | Non-Greasy | Makes Skin Glow",
+    img2:"",
+    rating:"3.5 stars |",
+    img_url:"",
+    review:"2 Reviews",
+    },
+]
+
+var onionRange = [
+    {
+        img:"https://images.mamaearth.in/catalog/product/o/n/onion_hair_oil_200ml_fop_1200x1200.png?format=auto&width=400&height=400",
+        rate:"Rs.",
+    price:299,
+    title:"Onion Hair Oil for Hair Regrowth and Hair Fall Control, 250ml",
+    subtitle:"Boosts Hair Growth | Adds Strength & Shine Hair",
+    img2:"",
+    rating:"3.5 stars |",
+    img_url:"",
+    review:"2 Reviews",
+    },
+    {
+        img:"https://images.mamaearth.in/catalog/product/o/n/onion-conditioner-ingredients.jpg?format=auto&width=400&height=400",
+        rate:"Rs.",
+    price:275,
+    title:"Onion Conditioner for Hair Fall Control - 250ml",
+    subtitle:"Reduces Hair Fall | Detangles Hair | Softens Hair",
+    img2:"",
+    rating:"3.5 stars |",
+    img_url:"",
+    review:"2 Reviews",
+    },
+    {
+        img:"https://images.mamaearth.in/catalog/product/o/n/onion-shampoo-600ml_1_1.jpg?format=auto&width=400&height=400",
+        rate:"Rs.",
+    price:449,
+    title:"Onion Shampoo with Onion & Plant Keratin for Hair Fall Control - 400ml",
+    subtitle:"Reduces Hair Fall | Strengthens Hair |Softens Hair",
+    img2:"",
+    rating:"3.5 stars |",
+    img_url:"",
+    review:"200 Reviews"    
+    },
+]
+
+var vitaminCRange = [
+    {img:"https://images.mamaearth.in/catalog/product/v/i/vitamin_c_face_toner_1.jpg?format=auto&width=400&height=400",
+    rate:"Rs.",
+    best:"Bestseller",
+price:399,
+title:"Vitamin C Face Toner with Vitamin C and Cucumber for Pore Tightening, 200 ml",
+subtitle:"Tightens Open Pores | Restores Skin's pH Balance",
+img2:"",
+rating:"4.5 stars |", 
+img_url:"",
+review:"90 Reviews",
+},
+{
+    img:"https://images.mamaearth.in/catalog/product/v/i/vitamin-c-oil-moisturizer-with-box-_-ingredients-copy.jpg?format=auto&width=400&height=400",
+    rate:"Rs.",
+price:399,
+title:"Vitamin C Oil-Free Moisturizer For Face with Vitamin C and Gotu Kola for Skin Illumination - 80 ml",
+subtitle:"Non-Sticky Moisturization | Restores Radiance",
+img2:"",
+rating:"4.5 stars |", 
+img_url:"",
+review:"42 Reviews",
+},
+{
+    img:"https://images.mamaearth.in/catalog/product/e/y/eye.-cream-copy.jpg?format=auto&width=400&height=400",
+    rate:"Rs.",
+price:299,
+title:"Vitamin C Under Eye Cream with Gotu Kola & Niacinamide for Brightening Under Eyes - 20 ml",
+subtitle:"Illuminates Under Eyes | Reduces Puffiness",
+img2:"",
+rating:"5 stars |", 
+img_url:"",
+review:"2 Reviews",
+},
+]
+
+var ubtanRange = [
+    {
+        img:"https://images.mamaearth.in/catalog/product/f/o/fop_with_ingredients.jpg?format=auto&width=400&height=400",
+        rate:"Rs.",
+    price:449,
+    title:"Ubtan Nourishing Hair Removal Kit Growth Reduction - 100g",
+    subtitle:"Removes Hair in 8 Minutes | Delays Hair Growth",
+    img2:"",
+    rating:"5 stars |", 
+    img_url:"",
+    review:"40 Reviews",
+    },
+    {
+        img:"https://images.mamaearth.in/catalog/product/u/b/ubtan_face_scrub_1.jpg?format=auto&width=400&height=400",
+        rate:"Rs.",
+    price:399,
+    title:"Ubtan Face Scrub with Turmeric and Walnut for Tan Removal - 100g",
+    subtitle:"Removes Tan | Gently Exfoliates | Evens Skin Tone",
+    img2:"",
+    rating:"3.5 stars |",
+    img_url:"",
+    review:"2 Reviews",
+    },
+    {
+        img:"https://images.mamaearth.in/catalog/product/u/b/ubtan-night-cream1.jpg?format=auto&width=400&height=380",
+        rate:"Rs.",
+    price:399,
+    title:"Ubtan Night Cream with Turmeric & Saffron for Skin Brightening – 50 g",
+    subtitle:"Brightens | Heals Damaged Skin| Gives Natural Glow",
+    img2:"",
+    rating:"3.5 stars |",
+    img_url:"",
+    review:"2 Reviews",
+    },
+]
+
+var hairCare = [
+    {
+        img:"https://images.mamaearth.in/catalog/product/b/h/bhringamla-shampoo_1_.jpg?format=auto&width=400&height=400",
+        rate:"Rs.",
+    price:449,
+    title:"BhringAmla Shampoo with Bhringraj and Amla for Intense Hair Treatment - 250 ml",
+    subtitle:"Repairs Damaged Hair | Boosts Hair Growth",
+    img2:"",
+    rating:"3.5 stars |",
+    img_url:"",
+    review:"200 Reviews",
+    
+    },
+    {
+        img:"https://images.mamaearth.in/catalog/product/r/i/rice-shampoo-with-ingredient_1_1_.jpg?format=auto&width=400&height=400",
+        rate:"Rs.",
+    price:429,
+    title:"Rice Water Shampoo For Damage Repair - 250ml",
+    subtitle:"Repairs Damaged Hair | Reduces Split Ends",
+    img2:"",
+    rating:"3.5 stars |",
+    img_url:"",
+    review:"28 Reviews",
+    },
+    {
+        img:"https://images.mamaearth.in/catalog/product/o/n/onion_hair_mask_1.jpg?format=auto&width=400&height=380",
+        rate:"Rs. ",
+    price:235,
+    title:"Onion Hair Mask, For Hair Fall Control, With Onion Oil and Organic Bamboo Vinegar, 200ml",
+    subtitle:"Reduces Hair Fall | Repairs Damaged Hair",
+    img2:"",
+    rating:"3.5 stars |",
+    img_url:"",
+    review:"27 Reviews",
+    },
+]
+
+
+// best seller
+localStorage.setItem("bestSellerdata",JSON.stringify(bestSeller));
+var bestSellerarr=JSON.parse(localStorage.getItem("bestSellerdata"))||[];
+// var productarr=JSON.parse(localStorage.getItem("products"))||[]; //product page part
+var container=document.querySelector(".product-container-content");
+  function display(data){
+    container.innerHTML=null;
+    data.forEach(function (elem){
+      var box=document.createElement("div");
+      box.addEventListener("click",function(){
+        productdis(elem);
+     })
+      var avatar=document.createElement("img");
+      // var buttn=document.createElement("button");
+      // buttn.innerText=elem.best;
+      var name=document.createElement("h2");
+      var subname=document.createElement("h3");
+      var ratingbox=document.createElement("div")
+      ratingbox.classList.add("modal");
+      var star=document.createElement("img");
+      var ratings=document.createElement("p");
+      var rightmark=document.createElement("img")
+      var reviws=document.createElement("p")
+      var pricebox=document.createElement("div")
+      var val=document.createElement("h1")
+      var price=document.createElement("h1");
+
+      pricebox.append(val,price)
+      console.log(pricebox)
+      ratingbox.append(star,ratings,rightmark,reviws)
+      var btn=document.createElement("button");
+      btn.innerText="ADD TO CART"
+       btn.addEventListener("click",function(){
+        addtocart(elem);
+     })
+     avatar.setAttribute("src",elem.img);
+     name.innerText=elem.title;
+     subname.innerText=elem.subtitle;
+     star.setAttribute("src",elem.img2);
+     ratings.innerText=elem.rating;
+     rightmark.setAttribute("src",elem.img_url)
+     reviws.innerText=elem.review;
+     val.innerText=elem.rate;
+     price.innerText=elem.price;
+    
+     box.append(avatar,name,subname,ratingbox,pricebox,btn);
+     document.querySelector(".product-container-content").append(box);
+    })
+  }
+  display (bestSeller);
+  console.log(bestSeller)
+
+     function addtocart(elem){
+    console.log(ele)
+       localStorage.setItem("cart",JSON.stringify(elem));
+       alert("Product Has been added successfully")
+     }
+ function pricesort(bestSeller){
+   var selected=document.querySelector("#SortBy").value;
+   console.log(selected)
+
+
+   if(selected=="Lowtohigh"){
+     console.log("hi")
+    bestSellerarr.sort(function (a,b){
+      return a.price - b.price;
+    });
+      console.log(bestSellerarr)
+      display(bestSellerarr)
+  }
+    if(selected=="Hightolow"){
+    bestSellerarr.sort(function (a,b){
+      return b.price - a.price;
+    });
+    console.log(bestSellerarr)
+    display(bestSellerarr)
+  
+   }
+  }
